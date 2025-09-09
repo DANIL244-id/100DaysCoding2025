9f8fdd880fdf8630c18d8bfa61283e2f66aefc1a
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-09 23:13:00 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,5 @@
+public class Day2 {
+  public static void main(String[] args) {
+    System.out.println("danil");
+    System.out.println(18);
+    System.out.printf("nama: %s, umur:%d %n","danil",18);
