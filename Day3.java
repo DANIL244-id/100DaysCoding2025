94fcdb85347db678c479ec993821356643ca098f
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-10 21:13:13 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,15 @@
+public class day3 {
+
+  public static void main(String[] args) {
+   // untuk komentar satu baris
+    /* untuk komentar lebih 
+         dari satu baris */
+
+   // program untuk menampilkan nama
+   /* baris dibawah ini akan menampilkan nama bulan */
+   System.out.println("september"); 
+
+
+
+    }
+}
