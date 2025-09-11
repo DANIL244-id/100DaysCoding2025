32ca442e70d4d2727abc69e7323863f57012a8a3
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-11 20:28:38 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,23 @@
+public class day4 {
+
+  public static void main(String[] args) {
+     // /n dipakai untuk membuat baris baru
+    System.out.println("baris pertama\nbaris kedua\nbaris ketiga");
+
+     // \t dipakai untuk menambahkan spasi tab (sama seperti menekan tombol tab di keyboard)
+    System.out.println("nama  \t  : DANIL");
+    System.out.println("alamat \t : desa baru");
+
+     // \" dipakai untuk menuliskan tanda kutip ganda (")
+    System.out.println("nama saya, \"DANIL\"");
+    System.out.println("alamat saya, \"desa baru\"");
+
+     /* \\ (blacklash)
+          fungsinya untuk menuliskan tanda blacklash(\)itu sendiri
+       */
+    System.out.println("Folder: C:\\Users\\day4");
+
+
+  }
+  
+}
