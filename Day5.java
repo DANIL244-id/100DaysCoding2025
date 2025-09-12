c82153ecb8ef5778795d1e8208ab7dd432efbeb2
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-12 20:14:09 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class day5 {
+
+public static void main(String[] args) {
+    byte umur = 18;
+    short tahun = 2025;
+    int password = 12345678;
+    long nomor = 6285236860044L;
+
+   System.out.println("umur");
+   System.out.println("tahun");
+   System.out.println("password");
+   System.out.println("nomor");
+
+   }
+
+}
