fb95bad6f2debb7f1361037169876551d07dc37c
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-13 20:34:43 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+public class day6 {
+ public static void main(String[] args) {
+   float nilai_a = 9.5f;
+   float nilai_b = 8.4f;
+
+  System.out.println("nilai_a = "+nilai_a);
+  System.out.println("nilai_b = "+nilai_b);
+
+   double angka_c = 100;
+   double angka_d = 200;
+
+  System.out.println("angka_c = "+angka_c);
+  System.out.println("angka_d = "+angka_d);
+
+  }
+
+}
