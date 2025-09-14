ead945d43c754dde05dacaaa8da1bcd61be0102e
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-14 20:36:13 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,16 @@
+public class day7 {
+ public static void main (String[] args) {
+   char angka = '7';
+   char huruf = 'd';
+   System.out.println("angka "+angka);
+   System.out.println("huruf "+huruf);
+   
+   boolean yes = true;
+   boolean no = false;
+   System.out.println("yes = " +yes);
+   System.out.println("no  = " +no);
+
+
+  }
+  
+}
