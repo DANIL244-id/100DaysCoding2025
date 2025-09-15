a046fdaa0f04c4ada8905d1178bd313c6e8d389f
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-15 20:34:58 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class day8 {
+  public static void main (String[] args) {
+     String nama = "DANIL";
+     String alamat = "Desa baru";
+     String umur = "umur 18tahun";
+
+   System.out.println("Nama   :" +nama);
+   System.out.println("Alamat :" +alamat);
+   System.out.println("Umur   :" +umur);
+
+
+  }
+
+}
