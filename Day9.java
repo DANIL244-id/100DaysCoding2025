fa016c29ee346ce6d8d3a68690b964a717f5f3a7
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-16 21:14:32 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,22 @@
+public class day9 {
+ public static void main (String[] args) {
+   String nama;
+   int kelas;
+
+   nama = "danil"
+   kelas = 13;
+
+   System.out.println("nama :" +nama);
+   System.out.println("kelas :" +kelas);
+
+
+   nama = "DANIL";
+   kelas = 19;
+
+   System.out.println("nama :" +nama);
+   System.out.println("kelas :" +kelas;
+
+
+   }
+
+}
