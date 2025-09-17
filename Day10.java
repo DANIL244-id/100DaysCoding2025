d3e33a6b5968774d602a10fe717be8ef4c5fd62d
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-17 21:08:19 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,28 @@
+public class day10 {
+  public static void main (String[] args) {
+    final String nama = "DANIL";
+    final byte b = 100;
+    final short s = 1000;
+    final int i = 10000;
+    final long l = 100000;
+    final float f = 10.1f;
+    final double d = 3.1234567;
+    final char c = 'c';
+    final boolean bt = true
+    final boolean bf = false
+
+    System.out.println("v_nama   :" +nama);
+    System.out.println("v_byte   :" +b);
+    System.out.println("v_short  :" +s);
+    System.out.println("v_int    :" +i);
+    System.out.println("v_long   :" +l);
+    System.out.println("v_float  :" +f);
+    System.out.println("v_double :" +d);
+    System.out.println("v_char   :" +c);
+    System.out.println("v_boolean:" +bt);
+    System.out.println("v_boolean:" +bf);
+
+
+   }
+
+}
