96a0ac6d46debdc5f68a11af85a2ab998e6bcb3c
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-18 22:24:04 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,22 @@
+public class day11 {
+ public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Masukkan nama : ");
+    String nama = input.nextLine();
+
+    System.out.println("Masukkan kelas : ");
+    int kelas = input.nextInt();
+
+    System.out.println("Masukkan f_angka : ");
+    int f_angka = input.nextInt();
+
+    System.out.println("nama saya :" +nama);
+    System.out.println("kelas     :" +kelas);
+    System.out.println("angka     :" +f_angka);
+
+
+  }
+
+
+}
