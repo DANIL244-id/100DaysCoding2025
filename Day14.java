186f06a51bafd1623d5e829bff924380cc0b51a7
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-21 20:40:57 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class day14  {
+  public static void main(String[] args) {
+    String str = "100";
+    int angka = Integer.parseInt(str);
+    String bln = "true";
+    boolean ya = Boolean.parseBoolean(bln);
+
+     System.out.println("angka : "+ angka);
+     System.out.println("ya    : "+ ya);
+
+
+  }
+
+}
