c36efd30167d97c6a12ae065cd91e6bbf81492da
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-22 17:39:29 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day15 {
+ public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+   System.out.println("Masukkan nilai a :");
+   int a = input.nextInt();
+   System.out.println("Masukkan nilai b :");
+   int b = input.nextInt();
+
+  System.out.println("nilai a :" +a);
+  System.out.println("nilai b :" +b);
+  System.out.println("Hasil penjumlahan :" + (a+b));
+  System.out.println("Hasil pengurangan :" + (a-b));
+
+
+ }
+
+}
