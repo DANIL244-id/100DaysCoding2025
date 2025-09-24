5dc3446542fccaf1fd67d5fabcf340091981d4c2
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-24 22:13:44 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class day17 {
+ public static void main(String[] args) {
+        int a = 7;
+        int b = 6;
+
+         int hasil = a % b;
+
+        System.out.println("Hasil : " + hasil);
+
+   }
+
+  
+}
