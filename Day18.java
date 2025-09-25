dc0a6dc6fff0ab4d5568880ed0f9ef733b8580f8
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-25 22:42:25 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class day18 {
+ public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   
+   System.out.println("masukkan int: );
+   int a_int = input.nextInt();
+
+  double a_double = a_int;
+
+  System.out.println("int ke double: "+a_double);
+
+ }
+
+}
