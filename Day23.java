8294537fb2c3f9b11cd9708f4fbbb61389624d16
DANIL244-id <danilanilmapilli22@gmail.com>
2025-09-30 21:57:59 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ public class day23 {
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan panjang: ");
+        int panjang = input.nextInt();
+        
+        System.out.print("Masukkan lebar: ");
+        int lebar = input.nextInt();
+
+        int luas = panjang * lebar;
+        System.out.println("Luas persegi panjang: ");
+
+  }
+
+ }
