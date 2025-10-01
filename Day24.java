b08c7e287f4b218aafccb1791dca52e2426e63b9
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-01 22:53:56 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class day24 {
+   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+      System.out.print("Masukkan jari-jari lingkaran: ");
+      double r = input.nextDouble();
+        
+      double luas = Math.PI * r * r;
+        
+      System.out.println("Luas lingkaran dengan jari-jari " + r + "adalah = " +luas);
+
+   }
+
+}
