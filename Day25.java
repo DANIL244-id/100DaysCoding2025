beb030dc08a36397a356879c84b6467b212fa38f
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-02 23:05:58 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,17 @@
+public class day25 {
+    public static void main(String[] args) {
+       //Increment (++)
+        int a = 9;
+        System.out.println("Nilai awal a = "+a);
+        a++;
+        System.out.println("Setelah Increment nilai a menjadi = "+a);
+        
+        //Decrement (--)
+        int b = 5;
+        System.out.println("Nilai awal b = "+b);
+        b--;
+        System.out.println("Setelah Decrement nilai b menjadi = "+b);
+
+    }
+
+}
