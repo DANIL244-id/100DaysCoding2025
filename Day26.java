807138c2aafd25ca08235037ad4b448214da6b76
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-03 20:07:17 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,26 @@
+public class day26 {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+
+        int b = 15;
+        b -= 5;
+
+        int c = 10;
+        c *= 10;
+
+        int d = 120;
+        d /= 2;
+
+        int e = 7;
+        e %= 6;
+
+        System.out.println("Hasil penjumlahan = " +a);
+        System.out.println("Hasil pengurangan = " +b);
+        System.out.println("Hasil perkalian   = " +c);
+        System.out.println("Hasil pembagian   = " +d);
+        System.out.println("Hasil modulus     = " +e);
+
+    }
+
+}
