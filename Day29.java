c7a387dc789993416e8131ca99da7306f5eb75b1
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-06 22:26:11 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day29 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nilai a: ");
+    int a = input.nextInt();
+    System.out.print("Masukkan nilai b: ");
+    int b = input.nextInt();
+
+    System.out.println("Hasil > : " + (a > b));
+    System.out.println("Hasil < : " + (a < b));
+
+  }
+
+}
