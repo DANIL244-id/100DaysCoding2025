72b4246031da00877662c9712a3154c93cfcb7ce
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-07 23:11:43 +0800

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -3,9 +3,9 @@ public class day30 {
   public static void main (String[] args) {
     
     Scanner input = new Scanner(System.in);
-    System.out.print("Masukkan nilai a: ");
+    System.out.print("Masukkan nilai c: ");
     int c = input.nextInt();
-    System.out.print("Masukkan nilai b: ");
+    System.out.print("Masukkan nilai d: ");
     int d = input.nextInt();
 
     System.out.println("Hasil >= : " + (c >= d));
