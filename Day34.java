1fbdb661459413a69bf9d4e01ec6f17ab058f5f8
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-11 23:15:10 +0800

    Create Day34.java

---
+++ Day34.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class day34 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nilai a: ");
+    int a = input.nextInt();
+    System.out.print("Masukkan nilai b: ");
+    int b = input.nextInt();
+    
+    System.out.println("Hasil : " + (a >= 80 && b != 20);
+
+  }
+
+}
