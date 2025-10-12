ed338cd84ed2bd01676b985cd0ccf971db4f099e
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-12 23:14:41 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class day35 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nilai : ");
+    int nilai = input.nextInt();
+    if (nilai < 0 || nilai > 100) {
+        System.out.println("NILAI TIDAK VALId!")
+    }else if (nilai >= 70) {
+        System.out.println("LULUS");
+    }else {
+        System.out.println("TIDAK LULUS");
+    }
+
+    
+  }
+
+}
