822b436809f5c05de424ee31161a7bd0734430ba
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-18 21:13:32 +0800

    Update Day41.java

--- Day41.java
+++ Day41.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 public class day41 {
   public static void main (String[] args) {
-    Scanner input = new Scanner(System.in);
         System.out.println("Pilihan menu");
         System.out.println("1. Nasi Goreng - Rp15.000");
-        System.out.println("1. Mie Goreng - Rp12.000");
-        System.out.println("1. Ayam Bakar - Rp18.000");
+        System.out.println("2. Mie Goreng - Rp12.000");
+        System.out.println("3. Ayam Bakar - Rp18.000");
         
         int nasi = 15000;
         int mie = 12000;
@@ -33,8 +32,15 @@ public class day41 {
                     System.out.println("total harga = "+total_diskon);
                 } 
                
-            }else{
-                System.out.println("total harga = "+t_porsi);
+            }else if (porsi <= 2) {
+                if (t_porsi > 50000){
+                    diskon = t_porsi * 0.05;
+                    total_diskon = t_porsi - diskon;
+                    System.out.println("total harga = "+total_diskon);
+                }else {
+                    System.out.println("total harga = "+t_porsi);
+                }
+                
             }
                 
         }else if (menu == 2){
@@ -52,8 +58,14 @@ public class day41 {
                     System.out.println("total harga = "+total_diskon);
                 } 
                
-            }else{
-                System.out.println("total harga = "+t_porsi);
+            }else if (porsi <= 2) {
+                if (t_porsi > 50000){
+                    diskon = t_porsi * 0.05;
+                    total_diskon = t_porsi - diskon;
+                    System.out.println("total harga = "+total_diskon);
+                }else {
+                    System.out.println("total harga = "+t_porsi);
+                }
             }
         }else if (menu == 3){
             System.out.println("Menu: Ayam Bakar");
@@ -70,9 +82,18 @@ public class day41 {
                     System.out.println("total harga = "+total_diskon);
                 } 
                
-            }else{
-                System.out.println("total harga = "+t_porsi);
+            }else if (porsi <= 2) {
+                if (t_porsi > 50000) {
+                    diskon = t_porsi * 0.05;
+                    total_diskon = t_porsi - diskon;
+                    System.out.println("total harga = "+total_diskon);
+                }else {
+                    System.out.println("total harga = "+t_porsi);
+                }
+                
             }
+        }else {
+           System.out.println("TIDAK VALID!");
         }
         
         
