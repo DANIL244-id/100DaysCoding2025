57aa7f051e6179d7507830bc7a160afc29d3adea
DANIL244-id <danilanilmapilli22@gmail.com>
2025-10-30 22:38:45 +0800

    Create Day53.java

---
+++ Day53.java
@@ -0,0 +1,15 @@
+public class day53 {
+  public static void main (String[] args) {
+    int i = 1;
+    while (i <= 10){
+      System.out.println("Nilai = "+i);
+      if (i == 5){
+        System.out.println("Berhenti di 5.");
+        break;
+      }
+      i++;
+    }
+
+  }
+
+}
