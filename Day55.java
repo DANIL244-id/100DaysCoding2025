f2be1498d540faa230ae1f0795921c061caa7cab
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-01 22:17:55 +0800

    Create Day55.java

---
+++ Day55.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class day55 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Batas Bawah : ");
+    int a = input.nextInt;
+    System.out.print("Batas Atas : ");
+    int b = input.nextInt;
+    System.out.print("Angka : ");
+    int c = input.nextInt;
+    if (c >= a && c <= b){
+      System.out.println("YAYAYAYA");
+    }else {
+      System.out.println("NONONONO");
+    }
+
+  }
+
+}
