2589851dab4070cd432b7ee2c008f6cd3ea98769
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-03 21:56:48 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,10 @@
+public class day57 {
+  public static void main (String[] args) {
+    for (int i = 1; i <= 2; i++) {
+      for (int c = 1; c <= 5; c++) {
+        System.out.println(c);
+      }
+    }
+  }
+
+}
