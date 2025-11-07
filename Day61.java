d9847fccd85127ab2bc6a279bd7330b6c9ee6641
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-07 22:52:49 +0800

    Create Day61.java

---
+++ Day61.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class day61 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nilai kelipatan M : ");
+    int m = input.nextInt();
+    System.out.print("Masukkan nilai N : ");
+    int n = input.nextInt();
+    System.out.println("\nBilangan kelipatan " +m +" dari 1 sampai " +n +": ");
+    for (int i = 1; i <= n; i++){
+      if (i % m == 0){
+        System.out.println(i + " ");
+      }
+    }
+
+  }
+}
