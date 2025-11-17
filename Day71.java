898d63d7be3afb222f5fb160636f95cce939d687
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-17 23:15:14 +0800

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class day71 {
+  public static void main (String[] args) {
+    Scanner p = new Scanner(System.in);
+        System.out.print("Masukkan tinggi(N): ");
+        int tinggi = p.nextInt();
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= i; j++) {
+               System.out.print("* ");
+            }
+          System.out.println();
+        }
+  }
+}
