66304bf6a8f17e99a8d90351098c1e04e2a5f6ea
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-23 22:59:47 +0800

    Create Day77.java

---
+++ Day77.java
@@ -0,0 +1,11 @@
+public class day77 {
+  public static void main (String[] args) {
+    String teks = " Meguru Java ";
+
+    System.out.println(teks.substring(1, 12));
+    System.out.println(teks.replace("Java", "Python"));
+    System.out.println(teks.trim());
+
+  }
+
+}
