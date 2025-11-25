51a140ff6ea75cc341016d7f7700cd0e3b283d0a
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-25 21:55:23 +0800

    Update Day79.java

--- Day79.java
+++ Day79.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class day79 {
   public static void main (String[] args) {
     Scanner p = new Scanner(System.in);
+    String user = "admin";
     String pass = "123";
     boolean login = false;
     while (!login){
+         System.out.print("Masukkan username: ");
+         String username = p.nextLine();
          System.out.print("Masukkan password: ");
          String password = p.nextLine();
-         if (password.equals(pass)) {
+         if (username.equals(user) && password.equals(pass)) {
            System.out.println("Login berhasil");
            login = true;
          }else{
-           System.out.println("Password salah. Coba lagi.");
+           System.out.println("Username atau password salah. Coba lagi.");
          }
 
     }
