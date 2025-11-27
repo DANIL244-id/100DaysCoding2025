24b43c0fecd4133a5cd88491434c58bb3ffdd0bd
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-27 23:20:48 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,12 @@
+public class day81 {
+  public static void main (String[] args) {
+    int[] angka = {5, 10, 15, 20, 25};
+   
+    System.out.println("index 0 : "+angka[0]);
+    System.out.println("index 1 : "+angka[1]);
+    System.out.println("index 2 : "+angka[2]);
+    System.out.println("index 3 : "+angka[3]);
+    System.out.println("index 4 : "+angka[4]);
+
+  }
+}
