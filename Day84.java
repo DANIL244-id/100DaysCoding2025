ad2e8975c33c601239d3dd9c351a95aecacbadcd
DANIL244-id <danilanilmapilli22@gmail.com>
2025-11-30 20:47:58 +0800

    Create Day84.java

---
+++ Day84.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class day84 {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+     String n = input.nextLine();
+     int angka = input.nextInt();
+
+     for (int i = 1; i <= angka; i++) {
+        System.out.println(n);
+     }
+    
+    }
+}
