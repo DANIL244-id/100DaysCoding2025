5fa506bfe8f61ebe81d498d722614610e6365322
DANIL244-id <danilanilmapilli22@gmail.com>
2025-12-04 22:28:31 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class day88 {
+  public static void main (String[] args) {
+    int[] angka = {11,22,33,44,55};
+    for (int b : angka) {
+         System.out.println(b);
+    }
+  }
+}
