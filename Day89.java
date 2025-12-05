b57d87a6abc81db2224d080d6a3e2efc6a2d134c
DANIL244-id <danilanilmapilli22@gmail.com>
2025-12-05 21:38:19 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class day89 {
+  public static void main (String[] args) {
+    salam();
+  }
+  public static void salam() {
+    System.out.println("Assalamualaikum");
+  }
+}
