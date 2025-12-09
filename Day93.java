22f1904da9f97871e3958d894c45e36d03c899be
DANIL244-id <danilanilmapilli22@gmail.com>
2025-12-09 22:18:06 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day93 {
+  public static void main (String[] args) {
+    double n = 4.6;
+
+    System.out.println("Ceil  : "+Math.ceil(n));
+    System.out.println("Floor : "+Math.floor(n));
+    System.out.println("Round : "+Math.round(n));
+  }
+}
