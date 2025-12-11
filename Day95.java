239e936c92f0360644c5e0896c834152f92ce0bf
DANIL244-id <danilanilmapilli22@gmail.com>
2025-12-11 22:55:05 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,8 @@
+public class day95 {
+  public static void main (String[] args) {
+     salam("Besok UAS");
+ }
+  public static void salam(String teks) {
+     System.out.println("Assalamualaikum "+teks);
+ }
+}
